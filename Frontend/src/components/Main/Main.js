211492cbaafdfb67ac2591c8e6ddb4e1f2572dab
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-04-13 12:47:57 +0200

    created basic meme details component
    modal working properly
    
    made some style for meme details with some sample datas

--- Frontend/src/components/Main/Main.js
+++ Frontend/src/components/Main/Main.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import setShowMemeDetailsAction from '../../actions/memeDetailsAction';
+import MemeDetails from '../MemeDetails/MemeDetails';
 
 const Main = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setShowMemeDetailsAction(true));
+  };
 
   return (
     <div id="main">
       <Switch>
         <Route exact path="/main">
           <h1>HELLO</h1>
+          <button onClick={handleClick}>EZ ITT EGY MEME</button>
+          <MemeDetails />
         </Route>
       </Switch>
     </div>
